b13f426e300fde36737ef29620382c3a99b91bc3
mogoatlhe <iam@ofent.se>
2022-12-13 15:54:19 +0200

    create file upload container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import File_Upload from './components/file_upload'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App flex justify-center items-center bg-white h-screen w-screen">
+      <File_Upload/>
     </div>
   )
 }
--- src/components/file_upload.tsx
+++ src/components/file_upload.tsx
@@ -1,7 +1,7 @@
 
 const File_Upload = () => {
-  return (<div>
-    
+  return (<div className = "w-80 h-64 bg-slate-900 rounded-xl p-1 grid">
+    <div className="border border-dashed border-slate-400 w-11/12 h-56 place-self-center rounded-lg"></div>
   </div>)
 }
 
