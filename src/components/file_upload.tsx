39cf9b392afdb2ab46bb6f576f9ceb4a30436302
mogoatlhe <iam@ofent.se>
2022-12-14 17:46:49 +0200

    add file upload text

--- src/components/file_upload.tsx
+++ src/components/file_upload.tsx
@@ -1,7 +1,14 @@
 
 const File_Upload = () => {
   return (<div className = "w-80 h-64 bg-slate-900 rounded-xl p-1 grid">
-    <div className="border border-dashed border-slate-400 w-11/12 h-56 place-self-center rounded-lg"></div>
+    <div className="flex flex-col p-6 items-center border border-dashed border-slate-400 w-11/12 h-56 place-self-center rounded-lg">
+      <p className="font-bold text-slate-400 text-center">Drag your documents here to start uploading</p>
+      <div className="flex gap-1 mt-4 ">
+        <div className="border-t border-slate-500 w-8 mt-[9px]"></div>
+        <p className="text-slate-500">OR</p>
+        <div className="border-t border-slate-500 w-8 mt-[9px]"></div>
+      </div>
+    </div>
   </div>)
 }
 
