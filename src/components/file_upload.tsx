f8305badf9ddc5270c60f58122ecfc6eab82ffe9
mogoatlhe <iam@ofent.se>
2022-12-14 22:40:47 +0200

    implement "get_files"

--- src/components/file_upload.tsx
+++ src/components/file_upload.tsx
@@ -1,5 +1,12 @@
 
 const File_Upload = () => {
+  const get_files = () => {
+    const files_btn = document.querySelector("#files") as HTMLElement | null;
+    
+    if(files_btn !== null){
+      files_btn.click()
+    }
+  }
   return (<div className = "w-80 h-64 bg-slate-900 rounded-xl p-1 grid">
     <div className="flex flex-col p-6 items-center border border-dashed border-slate-400 w-11/12 h-56 place-self-center rounded-lg">
       <p className="font-bold text-slate-400 text-center">Drag your documents here to start uploading</p>
@@ -12,7 +19,7 @@ const File_Upload = () => {
         <input type="file" id = "files" name = "files" multiple/>
         <input type="submit" id = "submit"/>
       </form>
-      <button className="font-bold bg-teal-900 mt-2">Browse files</button>
+      <button className="font-bold bg-teal-900 mt-2" onClick={get_files}>Browse files</button>
     </div>
   </div>)
 }
